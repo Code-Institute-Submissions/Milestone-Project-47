be42d58ab5f5ac9be3dc794e7f9cc59c9362f79d
WHusssain937 <wasim937@hotmail.com>
2020-04-10 11:13:28 +0000

    added maps and created folder for js

---
+++ assets/js/maps.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 6,
+        center: {
+            lat: 31.889901,
+            lng: -6.285022
+        }
+    });
+}   
